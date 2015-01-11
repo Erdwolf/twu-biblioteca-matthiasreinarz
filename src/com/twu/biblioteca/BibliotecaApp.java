78b68bcbd3fb508479ea293ba6a50dc8fce3f5b5
Matthias Reinarz <matthias.reinarz@gmail.com>
2015-01-11 16:43:25 +0000

    User Story: List of Books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,6 +1,5 @@
 package com.twu.biblioteca;
 
-import java.io.InputStream;
 import java.io.PrintStream;
 
 public class BibliotecaApp {
@@ -12,7 +11,9 @@ public class BibliotecaApp {
     }
 
     public void run() {
-        out.println("Welcome to Bibliotheca!");
+        out.println("Welcome to Biblioteca!");
+        out.println("Real World Haskell");
+        out.println("Java Persistence with Hibernate");
     }
 
     public static void main(String[] args) {
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,19 +1,36 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
-    @Test
-    public void testWelcomeMessage() {
+    private Scanner s;
+
+    @Before
+    public void captureOutput() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         new BibliotecaApp(new PrintStream(baos)).run();
-        assertEquals("Welcome to Bibliotheca!\n", baos.toString());
+        s = new Scanner(baos.toString());
     }
+
+    @Test
+    public void testWelcomeMessage() {
+        assertEquals("Welcome to Biblioteca!", s.nextLine());
+    }
+
+    @Test
+    public void testListOfBooks() {
+        s.nextLine();
+        assertEquals("Real World Haskell", s.nextLine());
+        assertEquals("Java Persistence with Hibernate", s.nextLine());
+    }
+
 }
