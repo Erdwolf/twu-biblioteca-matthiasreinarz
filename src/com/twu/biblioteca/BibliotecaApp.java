2aa2400e97b0df3ac767f1f0a60d013861408eb9
Matthias Reinarz <matthias.reinarz@gmail.com>
2015-01-11 16:33:13 +0000

    User story: Welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,22 @@
 package com.twu.biblioteca;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    private PrintStream out;
+
+    public BibliotecaApp(PrintStream out) {
+        this.out = out;
     }
+
+    public void run() {
+        out.println("Welcome to Bibliotheca!");
+    }
+
+    public static void main(String[] args) {
+        new BibliotecaApp(System.out).run();
+    }
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,18 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        new BibliotecaApp(new PrintStream(baos)).run();
+        assertEquals("Welcome to Bibliotheca!\n", baos.toString());
     }
 }
