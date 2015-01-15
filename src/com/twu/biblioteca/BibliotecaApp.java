5af4ba027422e96cb18780cf286ad0c73c1dc3cf
mreinarz <mreinarz@thoughtworks.com>
2015-01-15 14:37:44 +0000

    Basic console interface.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,35 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    private void run() {
+        Biblioteca biblioteca = new Biblioteca();
+
+        printWelcomeMessage();
+
+        while(true) {
+            try {
+                handleUserInput();
+            } catch(Exception e) {
+                System.out.println(e);
+            }
+        }
     }
+
+    private void printWelcomeMessage() {
+        System.out.println("Welcome to Biblioteca!");
+    }
+
+    private void handleUserInput() {
+        System.out.print("> ");
+        int menuOption = new Scanner(System.in).nextInt();
+    }
+
+    public static void main(String[] args) {
+        new BibliotecaApp().run();
+    }
+
 }
